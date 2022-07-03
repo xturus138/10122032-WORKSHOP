15f1591bfa71053ac89e16f41a5cb76fa883647e
fajarbh <hajargak@gmail.com>
2022-07-03 14:12:55 +0700

    update

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,7 @@
 module.exports = {
     index : async (req, res) => {
         loggedIn = req.session.loggedIn
-        res.render('index',{loggedIn});
+        req.session.user === undefined ? studentId = '' : studentId = req.session.user['student_id']
+        res.render('index',{loggedIn,studentId});
     }
 }
